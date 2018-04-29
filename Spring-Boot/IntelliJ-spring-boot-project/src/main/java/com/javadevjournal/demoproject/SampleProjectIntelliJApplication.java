545a7028d6c98c7b28ae63415a8572a010390488
Umesh Awasthi <umesh@pragiti.com>
2018-04-29 09:28:57 -0700

    How to create project with IntelliJ

---
+++ Spring-Boot/IntelliJ-spring-boot-project/src/main/java/com/javadevjournal/demoproject/SampleProjectIntelliJApplication.java
@@ -0,0 +1,12 @@
+package com.javadevjournal.demoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleProjectIntelliJApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleProjectIntelliJApplication.class, args);
+    }
+}
---
+++ Spring-Boot/IntelliJ-spring-boot-project/src/test/java/com/javadevjournal/demoproject/SampleProjectIntelliJApplicationTests.java
@@ -0,0 +1,16 @@
+package com.javadevjournal.demoproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SampleProjectIntelliJApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
