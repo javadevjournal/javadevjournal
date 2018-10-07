2c6e990640c3d6e491ba0f1c4f4291dec183bf1e
Dhananjay Singh <dhananjay1204@gmail.com>
2018-10-07 06:36:02 +0530

    Spring Boot Admin example

---
+++ Spring-Boot/spring-admin/spring-admin-server/src/main/java/com/example/springadminserver/SpringAdminServerApplication.java
@@ -0,0 +1,15 @@
+package com.example.springadminserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@SpringBootApplication
+@EnableAdminServer
+public class SpringAdminServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAdminServerApplication.class, args);
+	}
+}
