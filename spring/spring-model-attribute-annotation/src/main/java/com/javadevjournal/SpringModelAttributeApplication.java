080485dacd5d481532833311d6c0e2424015041c
Umesh Awasthi <UAwasthi@rccl.com>
2018-11-07 09:07:02 -0800

    Spring MVC @ModelAttribute Annotation

---
+++ spring/spring-model-attribute-annotation/src/main/java/com/javadevjournal/SpringModelAttributeApplication.java
@@ -0,0 +1,12 @@
+package com.javadevjournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringModelAttributeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringModelAttributeApplication.class, args);
+    }
+}
