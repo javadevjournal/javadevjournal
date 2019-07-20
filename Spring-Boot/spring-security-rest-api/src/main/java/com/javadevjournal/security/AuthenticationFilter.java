a4ede064a17f27cb249a35ad70839a37edcecba9
Umesh Awasthi <umeshawasthi@gmail.com>
2019-07-20 10:20:21 -0700

    Update AuthenticationFilter.java

--- Spring-Boot/spring-security-rest-api/src/main/java/com/javadevjournal/security/AuthenticationFilter.java
+++ Spring-Boot/spring-security-rest-api/src/main/java/com/javadevjournal/security/AuthenticationFilter.java
@@ -30,7 +30,7 @@ public class AuthenticationFilter extends AbstractAuthenticationProcessingFilter
     @Override
     public Authentication attemptAuthentication(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws AuthenticationException, IOException, ServletException {
 
-        Optional<String> tokenParam = Optional.ofNullable(httpServletRequest.getHeader(AUTHORIZATION)); //Authorization: Bearer TOKEN
+        //Optional<String> tokenParam = Optional.ofNullable(httpServletRequest.getHeader(AUTHORIZATION)); //Authorization: Bearer TOKEN
         String token= StringUtils.isNotEmpty(httpServletRequest.getHeader(AUTHORIZATION))? httpServletRequest.getHeader(AUTHORIZATION) : "";
         token= StringUtils.removeStart(token, "Bearer").trim();
         Authentication requestAuthentication = new UsernamePasswordAuthenticationToken(token, token);
