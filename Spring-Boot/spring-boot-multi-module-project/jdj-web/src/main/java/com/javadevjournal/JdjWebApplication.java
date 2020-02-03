68355b0ac91d6efdd9c86c0ab1abe30e90249c89
Umesh Awasthi <umeshawasthi@gmail.com>
2020-02-02 16:12:06 -0800

    Multi-Module Project With Spring Boot

---
+++ Spring-Boot/spring-boot-multi-module-project/jdj-web/src/main/java/com/javadevjournal/JdjWebApplication.java
@@ -0,0 +1,13 @@
+package com.javadevjournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdjWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdjWebApplication.class, args);
+	}
+
+}
