7c2cded63fd2047d6d54077542063d0ea4436d4a
Ayoosh Sharma <ayoosh.sharma80@gmail.com>
2020-02-17 22:46:18 -0800

    added comments

--- Spring-Boot/spring-application-properties/src/main/java/com/javadevjournal/SpringApplicationPropertiesApplication.java
+++ Spring-Boot/spring-application-properties/src/main/java/com/javadevjournal/SpringApplicationPropertiesApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringApplicationPropertiesApplication {
-    //MAIN APPLICATION
+    //main comment to test
 	public static void main(String[] args) {
 		SpringApplication.run(SpringApplicationPropertiesApplication.class, args);
 	}
