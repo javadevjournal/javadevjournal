d54e47ee7ba17a117c50fb08a57df201cd683786
Kunwar <kunwar.nitw@gmail.com>
2020-05-13 18:53:02 +0530

    Spring Cloud Netflix Eureka Server and Eureka Client

--- Spring-Boot/spring-cloud-eureka-client-server/eurekaclient2/src/main/java/com/javadevjournal/eurekaclient2/controller/Eurekaclient2Controller.java
+++ Spring-Boot/spring-cloud-eureka-client-server/eurekaclient2/src/main/java/com/javadevjournal/eurekaclient2/controller/Eurekaclient2Controller.java
@@ -1,41 +1,51 @@
 package com.javadevjournal.eurekaclient2.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import com.javadevjournal.eurekaclient2.model.Product;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 public class Eurekaclient2Controller {
+    static{
+        List<Product> productList = new ArrayList<>();
+        Product p1 = new Product(UUID.randomUUID(), "Guitar" , 99.99, "Music");
+        Product p2 = new Product(UUID.randomUUID(), "AWS Book" , 29.99, "Books");
+        Product p3 = new Product(UUID.randomUUID(), "Bread" , 9.99, "Food");
+        Product p4 = new Product(UUID.randomUUID(), "Soap" , 99.99, "Body");
+        productList.add(p1);
+        productList.add(p2);
+        productList.add(p3);
+        productList.add(p4);
+    }
 
-    @Autowired
-    private LoadBalancerClient loadBalancerClient;
-
-    private RestTemplate restTemplate = new RestTemplate();
-
-    @RequestMapping("/calleurekaclient2")
+    @GetMapping("/calleurekaclient2")
     public ResponseEntity<String> callEurekaClient2(){
         return new ResponseEntity<String>("Hello From Client 2", HttpStatus.OK);
     }
 
-    @RequestMapping("/callEurekaClient3viaClient2")
-    public ResponseEntity<String> callClient2(){
-        try {
-            return new ResponseEntity<String>(
-                    restTemplate.getForObject(getEurkaClient2BaseUri() + "/calleurekaclient3", String.class), HttpStatus.OK);
-        }catch (Exception exp) {
-            return new ResponseEntity<String>(
-                    restTemplate.getForObject(getEurkaClient2BaseUri() + "/calleurekaclient3", String.class), HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+    @GetMapping(value = "/allproducts", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Product> list() {
+        return Eurekaclient2Controller.productList;
     }
 
-    private String getEurkaClient2BaseUri(){
-        ServiceInstance serviceInstance =  loadBalancerClient.choose("EUREKACLIENT3");
-        return serviceInstance.getUri().toString();
+    @GetMapping(value = "/product/{id}" , produces = MediaType.APPLICATION_JSON_VALUE)
+    public Product showProduct(@PathVariable UUID id) {
+        return new Product(id, "Guitar" , 99.99, "Music");
+    }
+
+    @PostMapping(value = "/product/update")
+    public String saveProduct(@RequestBody Product product) {
+        return "Product with product id: "+ product.getProductId() +" and product name:"+product.getProductName()+" has been saved successfully!";
+    }
+
+    @DeleteMapping(value = "/product/delete/{id}")
+    public void delete(@PathVariable UUID id) {
+        //log "Product "+ id +" has been deleted successfully!";
     }
 }
 
